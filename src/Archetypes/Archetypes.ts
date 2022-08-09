440c0ebc85c0a900cd5aa360e08505cb2051b2a8
Leonidas <leonidas.drawanz@gmail.com>
2022-08-09 18:44:05 -0300

    feat: requisito 4, implementando a classe Archetypes

---
+++ src/Archetypes/Archetypes.ts
@@ -0,0 +1,28 @@
+import { EnergyType } from '../Energy';
+
+export default abstract class Archetypes {
+  private _special: number;
+  private _cost: number;
+  constructor(private _name: string) {
+    this._special = 0;
+    this._cost = 0;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get special() {
+    return this._special;
+  }
+
+  get cost() {
+    return this._cost;
+  }
+
+  static createdArchetypeInstances() {
+    throw new Error('Not implemented');
+  }
+
+  abstract get energyType(): EnergyType;
+}
